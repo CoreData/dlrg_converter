b059cceed37a943b337d2bd98dcf348730ca3ead
Chris Anders <github@hell-labs.org>
2013-09-16 12:34:15 +0200

    Changed a few typos in the file.

--- NAMParser_Individual.rb
+++ NAMParser_Individual.rb
@@ -9,19 +9,19 @@ class Swimmer
   attr_accessor :first_name, :last_name, :club, :category
   attr_reader :birth_year
 
-  def initialize(first_name = "", last_name = "", club = "", category = "")
+  def initialize(first_name = '', last_name = '', club = '', category = '')
     @first_name = first_name
     @last_name = last_name
     @club = club
     @category = category
-    @birthyear = "1990"
+    @birth_year = '1990'
   end
 end
 
 class Event
   attr_accessor :number, :name, :distance, :category, :heat
 
-  def initialize(number=0, name="", distance="", category="", heat={})
+  def initialize(number=0, name='', distance='', category='', heat={})
     @number = number
     @name = name
     @distance = distance    
@@ -48,16 +48,16 @@ class FileWriter
     @filename = filename
     @payload = payload
     
-    @scoreboard_dir = "_SCOREBOARD_"
-    @ares_dir = "_Ares_"
+    @scoreboard_dir = '_SCOREBOARD_'
+    @ares_dir = '_Ares_'
 
     Dir.mkdir(@scoreboard_dir) unless File.exists? @scoreboard_dir 
     Dir.mkdir(@ares_dir) unless File.exists? @ares_dir
 
-    if @filename == "STEUER.TXT" || @filename.match("NAM")
-      @filename = @scoreboard_dir << "/" << @filename
-    elsif @filename.match("LST")
-      @filename = @ares_dir << "/" << @filename
+    if @filename == 'STEUER.TXT' || @filename.match('NAM')
+      @filename = @scoreboard_dir << '/' << @filename
+    elsif @filename.match('LST')
+      @filename = @ares_dir << '/' << @filename
     else
       raise "We can't handle this filename."
     end
@@ -67,7 +67,7 @@ class FileWriter
 
   def write_file
     begin
-      file = File.open(@filename, "a:iso-8859-1")
+      file = File.open(@filename, 'a:iso-8859-1')
       file.write(@payload)
     rescue IOError => e
       puts "Error while writing #{@filename}: #{e}."
@@ -83,7 +83,7 @@ class Storage
     @key = 0
   end
   
-  def add candidate
+  def add (candidate)
     if self.stored? candidate
       candidate 
     else
@@ -93,16 +93,16 @@ class Storage
   end
   
   def list
-    @storage.each do |key, value|
+    @storage.each do |value|
       value
     end
   end
   
-  def stored? candidate
+  def stored? (candidate)
     @storage.value? candidate
   end
   
-  def storage_id candidate
+  def storage_id (candidate)
     @storage.each do |key, value|
       if value == candidate
         return key
@@ -121,11 +121,11 @@ end
 line = 1
 setting = Hash.new
 event = Event.new
-tournament = Tournament.new "Salzpokal DLRG", "2013-09-14 09:00"
+tournament = Tournament.new 'Salzpokal DLRG', '2013-09-14 09:00'
 styles = Storage.new
 distances = Storage.new
 
-raise "Please provide a filename as argument." unless ARGV.length > 0
+raise 'Please provide a filename as argument.' unless ARGV.length > 0
 
 CSV.foreach(ARGV[0], :headers => true, :col_sep => ',', :encoding => 'iso-8859-1:UTF-8') do |row|
   if line == 1
@@ -139,12 +139,11 @@ CSV.foreach(ARGV[0], :headers => true, :col_sep => ',', :encoding => 'iso-8859-1
       end
     end
   elsif line == 2
-    1.upto POOL_LANES do |i|
+    1.upto POOL_LANES do
       number = row[0]
-      category = row[1].split(" ")[2]
+      category = row[1].split(' ')[2]
       name = row[2]
-      club = row[3]
-      distance = row[2].split(" ")[0].strip
+      distance = row[2].split(' ')[0].strip
       event = Event.new number, name, distance, category
       styles.add event.name.match('\D+')[0][2, event.name.length]
       distances.add event.name.match('\d{1,3}')[0]
@@ -156,10 +155,10 @@ CSV.foreach(ARGV[0], :headers => true, :col_sep => ',', :encoding => 'iso-8859-1
     end
   elsif line == 3
     1.upto POOL_LANES do |i|
-      event.heat[i].category = row[2+i].split(" ").last unless row[2+i] == nil || row[2+i].length == 0
+      event.heat[i].category = row[2+i].split(' ').last unless row[2+i] == nil || row[2+i].length == 0
     end
   else
-    raise "We are on a line count > 3. This is wrong."
+    raise 'We are on a line count > 3. This is wrong.'
   end
 
   if line < 3
@@ -204,18 +203,18 @@ tournament.events.each do |event|
   # We need a certain entry for the event category in this file
   case event.category
   when 'w'
-    event_category = "weiblich"
+    event_category = 'weiblich'
   when 'm'
-    event_category = "männlich"
+    event_category = 'männlich'
   when 'gem.'
-    event_category = "mixed"
+    event_category = 'mixed'
   end
    #Format string: #EventNumber, #Count #Distance #Number #Category
   FileWriter.new("STEUER.TXT", sprintf("%-6s1 x%6s %-21s%s\r\n", event.number, event.name.match('\d*m')[0], event.name.match('\D+')[0][2,10], event_category))
 
 #  FileWriter.new "LSTRACE.TXT", sprintf("%s;%s;%s;%s;%s;%s;%s;\r\n", event.number, 0,0,1,styles.style_id(event.name.match('\D+')[0][2,event.name.length]),tournament.date,tournament.start_time+1) 
   # NAM File
-  filename = sprintf("%05d", event.number) << "001.NAM"
+  filename = sprintf('%05d', event.number) << '001.NAM'
   event.heat.each do |key, value|
     #puts "#{key.class}: #{value}"
     if value == nil
